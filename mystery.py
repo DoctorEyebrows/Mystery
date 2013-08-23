1a79fe8f8e2d5cfd285c0aaa561a6b3e5ef114cd
Philip Jackson <dr.eyebrows@gmail.com>
2013-08-23 18:56:32 +0100

    Started some crude GUI code.

--- mystery.py
+++ mystery.py
@@ -5,8 +5,36 @@ import logging
 
 class MyFrame(wx.Frame):
     def __init__(self,parent,ID,title):
-        wx.Frame.__init__(self,parent,ID,title)
+        wx.Frame.__init__(self,parent,ID,title,size=(500,300))
         self.Show(True)
+
+        self.textbox = wx.TextCtrl(parent=self,id=wx.ID_ANY,
+                                   style=wx.TE_MULTILINE|wx.TE_READONLY,
+                                   size=(300,300))
+        self.bRandom = wx.Button(parent=self, id=wx.ID_ANY,
+                                 label="Random")
+        self.bReveal = wx.Button(parent=self, id=wx.ID_ANY,
+                                 label="Reveal")
+
+        sizer1 = wx.BoxSizer(wx.HORIZONTAL)
+        sizer2 = wx.BoxSizer(wx.VERTICAL)
+        
+        sizer1.Add(self.textbox,0,wx.GROW)
+        sizer1.Add(sizer2,1,wx.GROW)
+
+        spacer1 = wx.StaticText(parent=self)
+        spacer2 = wx.StaticText(parent=self)
+        spacer3= wx.StaticText(parent=self)
+
+        sizer2.Add(spacer1,2)
+        sizer2.Add(self.bRandom,0,wx.ALIGN_CENTER)
+        sizer2.Add(spacer2,1)
+        sizer2.Add(self.bReveal,0,wx.ALIGN_CENTER)
+        sizer2.Add(spacer3,2)
+
+        self.SetSizer(sizer1)
+        self.SetAutoLayout(True)
+        sizer1.Fit(self)
     
 def main():
     logging.basicConfig(level=logging.INFO)
